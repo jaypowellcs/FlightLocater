5f7de340a20d10523443ab6e25b3c16b7da98d32
[James <[jay.powellcs@gmail.com]>
2024-09-30 09:13:46 -0500

    Models

---
+++ Server/Models/Booking.cs
@@ -0,0 +1,10 @@
+﻿namespace Server.Models
+{
+    public class Booking
+    {
+        public int booking_id { get; set; }
+        public string booking_ref { get; set; }
+        public int price { get; set; }
+        public DateTime update_ts { get; set; }
+    }
+}
---
+++ Server/Models/Passenger.cs
@@ -0,0 +1,15 @@
+﻿namespace Server.Models
+{
+    public class Passenger
+    {
+        public int passenger_id { get; set; }
+        public int booking_id { get; set; }
+        public string booking_ref { get; set; }
+        public int passenger_number { get; set; }
+        public required string first_name { get; set; }
+        public required string last_name { get; set; }
+        public int account_id { get; set; }
+        public DateTime update_ts { get; set; }
+
+    }
+}
