1aeb7d10d95581f7c30f0fb01d2a46ce307b58e8
[James <[jay.powellcs@gmail.com]>
2024-09-30 09:59:10 -0500

    Routes & Pages

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header/header.component";
 import { FooterComponent } from "./shared/footer/footer/footer.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { SignupComponent } from './pages/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SearchComponent } from './pages/search/search.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', redirectTo: '/home', pathMatch: 'full'},
+{path: 'home', component: HomeComponent},
+{path: 'login', component: LoginComponent},
+{path: 'search', component: SearchComponent},
+{path: 'signup', component: SignupComponent}
+];
+
+
