ba3cada203edfbfeead303f1db1ffff789e78e2d
[James <[jay.powellcs@gmail.com]>
2024-09-30 10:45:36 -0500

    Contact

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./shared/header/header/header.component";
 import { FooterComponent } from "./shared/footer/footer/footer.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { SignupComponent } from './pages/signup/signup.component';
+import { FormsModule } from '@angular/forms';  // <-- Make sure FormsModule is imported
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, SignupComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, SignupComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SearchComponent } from './pages/search/search.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
 {path: '', redirectTo: '/home', pathMatch: 'full'},
 {path: 'home', component: HomeComponent},
 {path: 'login', component: LoginComponent},
 {path: 'search', component: SearchComponent},
-{path: 'signup', component: SignupComponent}
+{path: 'signup', component: SignupComponent},
+{path: 'contact', component: ContactComponent},
 ];
 
 
