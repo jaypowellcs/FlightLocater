f919952510f2c0dbce00808e83541a385a56dd28
james <jay.powellcs@gmail.com>
2024-10-17 17:25:51 -0500

    login & signup form

--- Client/src/app/pages/login/login.component.ts
+++ Client/src/app/pages/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {  NgIf} from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Login Successful', this.loginForm.value);
+      // Add login logic here (e.g., API call)
+    } else {
+      console.log('Form Invalid');
+    }
+  }
 }
--- Client/src/app/pages/signup/signup.component.ts
+++ Client/src/app/pages/signup/signup.component.ts
@@ -1,12 +1,40 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [NgIf, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  signupForm: FormGroup;
+  passwordsDoNotMatch: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    });
+
+    this.signupForm.valueChanges.subscribe(() => {
+      this.validatePasswords();
+    });
+  }
+
+  validatePasswords(): void {
+    const { password, confirmPassword } = this.signupForm.value;
+    this.passwordsDoNotMatch = password !== confirmPassword;
+  }
+
+  onSubmit(): void {
+    if (!this.passwordsDoNotMatch) {
+      alert('Sign-up successful!');
+    }
+  }
 }
